ac28efc8bbf2a70b8ad7ee26c1c60c359228fb0e
VaniAman <amanvani7@gmail.com>
2024-09-29 20:18:28 +0530

    remove

--- src/app/model/studentviewmodel.model.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StudentService } from '../service/student.service';
-import { StudentViewModel } from './studentviewmodel.model';
-
-describe('Student', () => {
-  it('should create an instance', () => {
-    expect(new StudentService()).toBeTruthy();
-  });
-});
